2e98cb178b204be86d9e1a5f5ccddcd1fe42ac99
Your Name <you@example.com>
2025-01-09 19:32:56 +0700

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,35 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'screens/welcome_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/signup_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/message_screen.dart';
+import 'screens/profile_screen.dart';
+import 'screens/notification_screen.dart';
 
-void main() => runApp(createApp());
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Wireframe Project App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/welcome',
+      routes: {
+        '/welcome': (context) => WelcomeScreen(),
+        '/': (context) => LoginScreen(),
+        '/signup': (context) => SignupScreen(),
+        '/home': (context) => HomeScreen(),
+        '/messages': (context) => MessagesScreen(),
+        '/profile': (context) => ProfileScreen(),
+        '/notifications': (context) => NotificationsScreen(),
+      },
+    );
+  }
+}
